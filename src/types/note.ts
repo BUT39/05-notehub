1371178340cba0099231c4ee057046749756c13d
Dmytro But <butdmitrij25@mail.com>
2026-07-24 15:58:47 +0300

    upDate

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,10 +1,8 @@
-export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
-
 export interface Note {
-  id: string;
+  id: number;
   title: string;
   content: string;
   createdAt: string;
   updatedAt: string;
-  tag: NoteTag;
+  tag: string;
 }
